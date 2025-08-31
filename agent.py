b854f4e66bc6d4ad6937ece90a88782e027a3090
msumari <suleykhery@gmail.com>
2025-08-31 21:27:54 +0100

    feat:Deployed agent to agentcore runtime

--- agent.py
+++ agent.py
@@ -1,6 +1,16 @@
+from bedrock_agentcore.runtime import BedrockAgentCoreApp
 from strands import Agent
 
-
+app = BedrockAgentCoreApp()
 agent = Agent(model="anthropic.claude-3-haiku-20240307-v1:0")
 
-agent("Tell me about agentic AI")
+
+@app.entrypoint
+def invoke(payload):
+    user_input = payload.get("prompt", "Tell me about agentic AI")
+    result = agent(user_input)
+    return {"result": result.message}
+
+
+if __name__ == "__main__":
+    app.run()
